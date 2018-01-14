1a336b6aab0c5fa62332baa573aab662cdfb7613
James Getrfost <jms@gtrst.me>
2018-01-14 16:05:24 -0600

    Initial Commit.

---
+++ jetstreamsgo/Data/LibraryContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static jetstreamsgo.Models.LibraryModel;
+
+namespace jetstreamsgo.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Book> Book { get; set; }
+
+        public DbSet<Publisher> Publisher { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=library;user=library;password=books");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Publisher>(entity =>
+            {
+                entity.HasKey(e => e.ID);
+                entity.Property(e => e.Name).IsRequired();
+            });
+
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.HasKey(e => e.ISBN);
+                entity.Property(e => e.Title).IsRequired();
+                entity.HasOne(d => d.Publisher)
+                  .WithMany(p => p.Books);
+            });
+        }
+    }
+}
---
+++ jetstreamsgo/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using jetstreamsgo.Data;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using static jetstreamsgo.Models.LibraryModel;
+
+namespace jetstreamsgo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            InsertData();
+            PrintData();
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+
+        private static void InsertData()
+        {
+            using (var context = new LibraryContext())
+            {
+                // Creates the database if not exists
+                context.Database.EnsureCreated();
+
+                // Adds a publisher
+                var publisher = new Publisher
+                {
+                    Name = "Mariner Books"
+                };
+                context.Publisher.Add(publisher);
+
+                // Adds some books
+                context.Book.Add(new Book
+                {
+                    ISBN = "978-0544003415",
+                    Title = "The Lord of the Rings",
+                    Author = "J.R.R. Tolkien",
+                    Language = "English",
+                    Pages = 1216,
+                    Publisher = publisher
+                });
+                context.Book.Add(new Book
+                {
+                    ISBN = "978-0547247762",
+                    Title = "The Sealed Letter",
+                    Author = "Emma Donoghue",
+                    Language = "English",
+                    Pages = 416,
+                    Publisher = publisher
+                });
+
+                // Saves changes
+                context.SaveChanges();
+            }
+        }
+
+        private static void PrintData()
+        {
+            // Gets and prints all books in database
+            using (var context = new LibraryContext())
+            {
+                var books = context.Book
+                  .Include(p => p.Publisher);
+                foreach (var book in books)
+                {
+                    var data = new StringBuilder();
+                    data.AppendLine($"ISBN: {book.ISBN}");
+                    data.AppendLine($"Title: {book.Title}");
+                    data.AppendLine($"Publisher: {book.Publisher.Name}");
+                    Console.WriteLine(data.ToString());
+                }
+            }
+        }
+    }
+}
